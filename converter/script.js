5dfa305f55e5453e732f435e1bd10c09f3cbad00
Bruno Becoski <brunobecoski@gmail.com>
2023-11-24 23:13:26 -0300

    Atualizando o converter

--- converter/script.js
+++ converter/script.js
@@ -9,64 +9,117 @@
 //   alert("R$ " + valueInReal);
 // }
 
-let selectedMode = document.getElementById("mode_selection").value;
+const currencies = [
+  {
+    value: 'ars',
+    name: 'Peso argentino',
+    calc: {
+      'brl': 0.014,
+      'cny': 0.020,
+      'eur': 0.0026,
+      'usd': 0.0028,
+    },
+  },
+  {
+    value: 'brl',
+    name: 'Real brasileiro',
+    calc: {
+      'ars': 0.014,
+      'cny': 0.68,
+      'eur': 5.36,
+      'usd': 4.91,
+    },
+  },
+  {
+    value: 'cny',
+    name: 'Yuan chinês',
+    calc: {
+      'ars': 48.78,
+      'brl': 0.68,
+      'eur': 0.13,
+      'usd': 0.14,
+    },
+  },
+  {
+    value: 'eur',
+    name: 'Euro',
+    calc: {
+      'ars': 0.0026,
+      'brl': 0.19,
+      'cny': 0.13,
+      'usd': 0.92,
+    },
+  },
+  {
+    value: 'usd',
+    name: 'Dólar americano',
+    calc: {
+      'ars': 0.0028,
+      'brl': 0.20,
+      'cny': 0.14,
+      'eur': 1.09,
+    },
+  }
+]
+
+let select_element_1 = document.getElementById("select_1");
+let select_element_2 = document.getElementById("select_2");
+
+let input_element_1 = document.getElementById("input_1");
+let input_element_2 = document.getElementById("input_2");
+
+handleSelectMode();
 
 function handleSelectMode() {
-  selectedMode = document.getElementById("mode_selection").value;
+  const selectedMode = document.getElementById("mode_selection").value;
+
+  switch (selectedMode) {
+    case "currency":
+      fillSelects(currencies);
+      break;
+      
+    case "temperature":
+      console.log("Temperatura");
+      break;
+        
+    case "length":
+      console.log("Comprimento");
+      break;
+      
+    default:  "currency"
+      fillSelects(currencies);
+  }
 }
 
-// const currencies = [
-//   {
-//     code: 'brl',
-//     name: 'Real brasileiro',
-//     value: {
-//       'ars': 0.014,
-//       'cny': 0.68,
-//       'eur': 5.36,
-//       'usd': 4.91,
-//     },
-//   },
-//   {
-//     code: 'usd',
-//     name: 'Dólar americano',
-//     value: {
-//       'ars': 0.0028,
-//       'brl': 0.20,
-//       'cny': 0.14,
-//       'eur': 1.09,
-//     },
-//   },
-//   {
-//     code: 'eur',
-//     name: 'Euro',
-//     value: {
-//       'ars': 0.0026,
-//       'brl': 0.19,
-//       'cny': 0.13,
-//       'usd': 0.92,
-//     },
-//   },
-//   {
-//     code: 'cny',
-//     name: 'Yuan chinês',
-//     value: {
-//       'ars': 48.78,
-//       'brl': 0.68,
-//       'eur': 0.13,
-//       'usd': 0.14,
-//     },
-//   },
-//   {
-//     code: 'ars',
-//     name: 'Peso argentino',
-//     value: {
-//       'brl': 0.014,
-//       'cny': 0.020,
-//       'eur': 0.0026,
-//       'usd': 0.0028,
-//     },
-//   }
-// ]
+function fillSelects(items) { 
+  createOptions(items).forEach((option, index) => {
+    if (index === 0) {
+      option.selected = true;
+      select_element_1.appendChild(option);
+    } else {
+      select_element_1.appendChild(option);
+    }
+  });
+    
+  createOptions(items).forEach((option, index) => {
+    if (index === 1) {
+      option.selected = true;
+      select_element_2.appendChild(option);
+    } else {
+      select_element_2.appendChild(option);
+    }
+  });
+}
+
+function createOptions(items) {
+  return items.map((item) => {
+    const option = document.createElement("option")
+    option.innerText = item.name;
+    option.value = item.value;
+
+    return option;
+  })
+}
 
 // function handleInput(value, item) {
 //   if (item === 'first') {
