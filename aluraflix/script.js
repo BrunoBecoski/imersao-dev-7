c936b5f7b9419072b23aa577d3d85f532ecaed37
Bruno Becoski <brunobecoski@gmail.com>
2023-12-19 22:26:15 -0300

    Atualizando a aluraflix

--- aluraflix/script.js
+++ aluraflix/script.js
@@ -9,17 +9,37 @@
 
 addEventListener("submit", (event) => {
   event.preventDefault();
-  
-  const title = event.target["mediaTitle"].value;
-  const cover = event.target["mediaCover"].value;
 
+  const titleInput_element = event.target["mediaTitle"];
+  const coverInput_element = event.target["mediaCover"];
+  const videoInput_element = event.target["mediaVideo"];
+  
+  const titleValue = titleInput_element.value;
+  const coverValue = coverInput_element.value;
+  const videoValue = videoInput_element.value;
+
+  if (!titleValue || !coverValue || !videoValue) {
+    return;
+  }
+  
   const mediaList_element = document.getElementById("mediaList");
 
-  const title_element = document.createElement("h3");
-  const cover_element = document.createElement("img");
+  const div = document.createElement("div");
+  const h3_element = document.createElement("h3");
+  const img_element = document.createElement("img");
+  const iframe_element = document.createElement("iframe");
+  
+  const urlVideo = videoValue.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
+  
+  h3_element.innerText = titleValue;
+  img_element.src = coverValue;
+  iframe_element.src = urlVideo;
 
-  title_element.innerText = title;
-  cover_element.src = cover;
+  div.append(h3_element, img_element, iframe_element);
 
-  mediaList_element.append(title_element, cover_element);
+  mediaList_element.append(div);
+
+  titleInput_element.value = "";
+  coverInput_element.value = "";
+  videoInput_element.value = "";
 });
