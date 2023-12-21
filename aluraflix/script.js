4d40e0ec855c8826c6c32a2f3283c7933a5c94ad
Bruno Becoski <brunobecoski@gmail.com>
2023-12-20 21:03:17 -0300

    Atualizando a aluraflix

--- aluraflix/script.js
+++ aluraflix/script.js
@@ -24,22 +24,34 @@ addEventListener("submit", (event) => {
   
   const mediaList_element = document.getElementById("mediaList");
 
-  const div = document.createElement("div");
+  const div_element = document.createElement("div");
   const h3_element = document.createElement("h3");
   const img_element = document.createElement("img");
   const iframe_element = document.createElement("iframe");
-  
+  const buttonRemove_element = document.createElement("button");
+
+  const id = titleValue.toLowerCase().replaceAll(/[^a-z0-9]+/ig, "_");
+
+  div_element.id = id;
+
+  buttonRemove_element.onclick = () => handleRemove(id);
+  buttonRemove_element.innerText = "Remover";
+
   const urlVideo = videoValue.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
   
   h3_element.innerText = titleValue;
   img_element.src = coverValue;
   iframe_element.src = urlVideo;
 
-  div.append(h3_element, img_element, iframe_element);
+  div_element.append( buttonRemove_element, h3_element, img_element, iframe_element);
 
-  mediaList_element.append(div);
+  mediaList_element.append(div_element);
 
   titleInput_element.value = "";
   coverInput_element.value = "";
   videoInput_element.value = "";
 });
+
+function handleRemove(value) {
+  document.getElementById(value).remove();
+}
