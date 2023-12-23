b7eca26ec4b45c36cd95de3f34fd8eb63644c074
Bruno Becoski <brunobecoski@gmail.com>
2023-12-22 21:30:59 -0300

    Atualizando a aluraflix

--- aluraflix/script.js
+++ aluraflix/script.js
@@ -32,21 +32,28 @@ addEventListener("submit", (event) => {
   const img_element = document.createElement("img");
   const iframe_element = document.createElement("iframe");
   const buttonRemove_element = document.createElement("button");
+  const buttonEdit_element = document.createElement("button");
 
   const id = titleValue.toLowerCase().replaceAll(/[^a-z0-9]+/ig, "_");
-
   div_element.id = id;
 
-  buttonRemove_element.onclick = () => handleRemove(id);
   buttonRemove_element.innerText = "Remover";
+  buttonRemove_element.onclick = () => handleRemove(id);
+
+  buttonEdit_element.innerText = "Editar";
+  buttonEdit_element.onclick = () => handleEdit(id);
 
   const urlVideo = videoValue.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
   
+  h3_element.id = "title";
+  img_element.id = "cover";
+  iframe_element.id = "video";
+
   h3_element.innerText = titleValue;
   img_element.src = coverValue;
   iframe_element.src = urlVideo;
 
-  div_element.append( buttonRemove_element, h3_element, img_element, iframe_element);
+  div_element.append(buttonRemove_element, buttonEdit_element, h3_element, img_element, iframe_element);
 
   divList_element.append(div_element);
 
@@ -58,14 +65,34 @@ addEventListener("submit", (event) => {
   mediaForm_element.style.display = "none";
 });
 
-function handleRemove(id) {
-  document.getElementById(id).remove();
-}
-
 function handleShow() {
   const buttonShow_element = document.getElementById("buttonShow");
   const formMedia_element = document.getElementById("formMedia");
 
   formMedia_element.style.display = "block";
   buttonShow_element.style.display = "none";
+}
+
+function handleRemove(id) {
+  document.getElementById(id).remove();
+}
+
+function handleEdit(id) {
+  const div_element = document.getElementById(id);
+  
+  const h3 = document.getElementById("title").innerText;
+  const img = document.getElementById("cover").src;
+  const iframe = document.getElementById("video").src;
+  
+  div_element.remove();
+  
+  const inputTitle_element = document.getElementById("inputTitle");
+  const coverInput_element = document.getElementById("inputCover");
+  const videoInput_element = document.getElementById("inputVideo");
+
+  inputTitle_element.value = h3;
+  coverInput_element.value = img;
+  videoInput_element.value = iframe;
+
+  handleShow();
 }
