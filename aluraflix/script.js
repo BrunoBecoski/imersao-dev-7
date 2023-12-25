c8ba1ad124173bffbf387222e9ace42998437cfe
Bruno Becoski <brunobecoski@gmail.com>
2023-12-25 14:26:45 -0300

    Atualizando a aluraflix

--- aluraflix/script.js
+++ aluraflix/script.js
@@ -7,50 +7,44 @@
 //   document.getElementById("movie").value = "";
 // }
 
-addEventListener("submit", (event) => {
-  event.preventDefault();
-  
-  const divMedia_element = createDivMedia();
-
-  if (!divMedia_element)  {
-    return;
-  }
-
-  setMediaList(divMedia_element);
-
-  const {
-    titleInput_element,
-    coverInput_element,
-    videoInput_element, 
-  } = getFormInputs();
-    
-  titleInput_element.value = "";
-  coverInput_element.value = "";
-  videoInput_element.value = "";
-
-  handleHideForm();
-});
-
-function setMediaList(divMedia_element) {
-  const divList_element = document.getElementById("divList");
-  divList_element.append(divMedia_element);
-}
-
-function createDivMedia() {
+function handleSubmit() {
   const {
     titleInput_element,
     coverInput_element, 
     videoInput_element,
   } = getFormInputs();
 
-  const titleValue = titleInput_element.value.trim();
-  const coverValue = coverInput_element.value.trim();
-  const videoValue = videoInput_element.value.trim();
+  const title = titleInput_element.value.trim();
+  const cover = coverInput_element.value.trim();
+  const video = videoInput_element.value.trim();
+
+  console.log(cover);
   
-  if (!titleValue || !coverValue || !videoValue) {
+  if (!title|| !cover|| !video) {
     return 
   }
- 
+
+  const divMedia_element = createDivMedia(title, cover, video);
+
+  setMediaList(divMedia_element);
+  
+  clearForm();
+  handleHideForm();
+}
+
+function setMediaList(divMedia_element) {
+  const divList_element = document.getElementById("divList");
+  divList_element.append(divMedia_element);
+}
+
+function handleSubmitEdit(id) {
+  handleSubmit();
+
+  const div_element = document.getElementById(id);
+  div_element.remove();
+}
+
+function createDivMedia(title, cover, video) {
   const div_element = document.createElement("div");
   const h3_element = document.createElement("h3");
   const img_element = document.createElement("img");
@@ -58,7 +52,7 @@ function createDivMedia() {
   const buttonRemove_element = document.createElement("button");
   const buttonEdit_element = document.createElement("button");
 
-  const id = titleValue.toLowerCase().replaceAll(/[^a-z0-9]+/ig, "_");
+  const id = title.toLowerCase().replaceAll(/[^a-z0-9]+/ig, "_");
   div_element.id = id;
 
   buttonRemove_element.innerText = "Remover";
@@ -67,14 +61,14 @@ function createDivMedia() {
   buttonEdit_element.innerText = "Editar";
   buttonEdit_element.onclick = () => handleEditMedia(id);
 
-  const videoUrl = videoValue.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
+  const videoUrl = video.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
   
   h3_element.id = "title";
   img_element.id = "cover";
   iframe_element.id = "video";
 
-  h3_element.innerText = titleValue;
-  img_element.src = coverValue;
+  h3_element.innerText = title;
+  img_element.src = cover;
   iframe_element.src = videoUrl;
 
   div_element.append(buttonRemove_element, buttonEdit_element, h3_element, img_element, iframe_element);
@@ -100,11 +94,22 @@ function handleShowForm() {
   document.getElementById("divList").style.display = "none";
 }
 
-function handleHideForm() {
+function handleHideForm(type) {
   document.getElementById("buttonShowForm").style.display = "inline-block";
   document.getElementById("formMedia").style.display = "none";
   document.getElementById("divList").style.display = "block";
 
+  if (type === "create") {
+    clearForm();
+    return;
+  }
+
+  if (type === "edit") {
+    return;
+  }
+}
+
+function clearForm() {
   const {
     titleInput_element,
     coverInput_element, 
@@ -121,9 +126,13 @@ function handleRemoveMedia(id) {
 }
 
 function handleEditMedia(id) {
-  const div_element = document.getElementById(id);
+  const buttonCancel_element = document.getElementById("buttonCancel");
+  buttonCancel_element.onclick = () => handleHideForm("edit");
 
-  div_element.remove();
+  const buttonSubmitEdit_element = document.getElementById("buttonSubmitEdit");
+  buttonSubmitEdit_element.onclick = () => handleSubmitEdit(id);
+
+  const div_element = document.getElementById(id);
 
   const title = div_element.querySelector("#title").innerText;
   const cover = div_element.querySelector("#cover").src;
@@ -138,6 +147,19 @@ function handleEditMedia(id) {
   titleInput_element.value = title;
   coverInput_element.value = cover;
   videoInput_element.value = video;
-
+  
+  changeButtonsForm("edit");
   handleShowForm();
+}
+
+function changeButtonsForm(type) {
+  if (type === "create") {
+    document.getElementById("buttonsCreate").style.display = "block";
+    document.getElementById("buttonsEdit").style.display = "none";
+  }
+
+  if (type === "edit") {
+    document.getElementById("buttonsEdit").style.display = "block";
+    document.getElementById("buttonsCreate").style.display = "none";
+  }
 }
